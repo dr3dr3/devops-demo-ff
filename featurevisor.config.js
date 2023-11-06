54c3a767fe0f201d1e3ea329a181db1c7e89ed72
André Dreyer <git@andredreyer.com>
2023-11-06 12:40:19 +1000

    Change environments to support blue/green

--- featurevisor.config.js
+++ featurevisor.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@featurevisor/core').ProjectConfig} */
 module.exports = {
-  environments: ["ci", "stg", "can", "prd"],
+  environments: ["ci", "stg", "blue", "green"],
   tags: ["all"],
   prettyState: true,
 };
